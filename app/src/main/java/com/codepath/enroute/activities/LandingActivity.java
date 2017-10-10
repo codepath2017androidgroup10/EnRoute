fe6581ab54aec0bb4429c931363c23152148e18f
Valli Vidhya Venkatesan <vallividhyavenkatesan@gmail.com>
2017-10-10 15:45:46 -0700

    Landing activity

--- app/src/main/java/com/codepath/enroute/activities/LandingActivity.java
+++ app/src/main/java/com/codepath/enroute/activities/LandingActivity.java
@@ -2,16 +2,26 @@ package com.codepath.enroute.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.codepath.enroute.R;
 import com.crashlytics.android.Crashlytics;
 import io.fabric.sdk.android.Fabric;
-public class MainActivity extends AppCompatActivity {
+public class LandingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Fabric.with(this, new Crashlytics());
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_landing);
+    }
+
+    /*
+    * Click Listener for Search button. Opens the map view on click.
+    *
+    * */
+
+    public void onButtonSearch(View view) {
+
     }
 }
