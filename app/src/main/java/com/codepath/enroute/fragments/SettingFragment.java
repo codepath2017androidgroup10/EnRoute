d3fce504fed065cd775e43dfbeab36f74fa68389
DroidSoul <DroidSoul@users.noreply.github.com>
2017-10-12 15:46:06 -0700

    Added setting setting in menu, fragment UI and clickable. fragment codes not done yet

---
+++ app/src/main/java/com/codepath/enroute/fragments/SettingFragment.java
@@ -0,0 +1,36 @@
+package com.codepath.enroute.fragments;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.codepath.enroute.R;
+
+public class SettingFragment extends DialogFragment{
+
+
+    public SettingFragment() {
+
+    }
+
+    public static SettingFragment newInstance() {
+
+        return new SettingFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_setting, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
