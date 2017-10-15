74ef942f170a64aa8c5a91e12a74fa5a3366ade6
DroidSoul <huangw2@gmail.com>
2017-10-14 20:02:35 -0700

    adding recycleview

--- app/src/main/java/com/codepath/enroute/activities/ListActivity.java
+++ app/src/main/java/com/codepath/enroute/activities/ListActivity.java
@@ -2,14 +2,77 @@ package com.codepath.enroute.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.codepath.enroute.R;
+import com.codepath.enroute.adapters.RestaurantAdapter;
+import com.codepath.enroute.connection.YelpClient;
+import com.codepath.enroute.models.Restaurant;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.Header;
 
 public class ListActivity extends AppCompatActivity {
+
+    RecyclerView rvRestaurants;
+    ArrayList<Restaurant> restaurants;
+    RestaurantAdapter restaurantAdapter;
+    YelpClient yelpClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+        rvRestaurants = findViewById(R.id.rvRestaurants);
+        restaurants = new ArrayList<>();
+        restaurantAdapter = new RestaurantAdapter(this, restaurants);
+        rvRestaurants.setAdapter(restaurantAdapter);
+        rvRestaurants.setLayoutManager(new LinearLayoutManager(this));
+        fetchData();
+    }
+    public void fetchData() {
+        yelpClient = YelpClient.getInstance();
+        RequestParams params = new RequestParams();
+        params.put("term","restaurants");
+        params.put("location","880 West Maude Avenue, Sunnyvale, CA");
+        yelpClient.getSearchResult(params, new JsonHttpResponseHandler(){
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                ArrayList<Restaurant> list = new ArrayList<>();
+                try {
+                    list = Restaurant.fromJSONArray(response.getJSONArray("businesses"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                restaurants.clear();
+                restaurants.addAll(list);
+                restaurantAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                super.onSuccess(statusCode, headers, response);
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, String responseString) {
+                super.onSuccess(statusCode, headers, responseString);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                super.onFailure(statusCode, headers, responseString, throwable);
+            }
+        });
+
+
     }
 }
---
+++ app/src/main/java/com/codepath/enroute/adapters/RestaurantAdapter.java
@@ -0,0 +1,103 @@
+package com.codepath.enroute.adapters;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import com.codepath.enroute.R;
+import com.codepath.enroute.models.Restaurant;
+import com.squareup.picasso.Picasso;
+
+import java.text.DecimalFormat;
+import java.util.List;
+
+import jp.wasabeef.picasso.transformations.RoundedCornersTransformation;
+
+/**
+ * Created by bear&bear on 10/14/2017.
+ */
+
+public class RestaurantAdapter extends RecyclerView.Adapter<RestaurantAdapter.ViewHolder>{
+
+    private List<Restaurant> mRestaurants;
+    private Context mContext;
+
+    public RestaurantAdapter(Context context, List<Restaurant> restaurants) {
+        this.mRestaurants = restaurants;
+        mContext = context;
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView ivProfileImage;
+        TextView tvName;
+        RatingBar ratingBar;
+        TextView tvOpen;
+        TextView tvDistance;
+        TextView tvPrice;
+        TextView tvReviewCount;
+        TextView tvCategory;
+        TextView tvAddress;
+
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            ivProfileImage = (ImageView) itemView.findViewById(R.id.ivProfileImage);
+            tvName = (TextView) itemView.findViewById(R.id.tvName);
+            ratingBar = itemView.findViewById(R.id.ratingBar);
+            tvOpen = (TextView) itemView.findViewById(R.id.tvOpen);
+            tvDistance = (TextView) itemView.findViewById(R.id.tvDistance);
+            tvPrice = (TextView) itemView.findViewById(R.id.tvPrice);
+            tvReviewCount = itemView.findViewById(R.id.tvReviewCount);
+            tvCategory = itemView.findViewById(R.id.tvCategory);
+            tvAddress = itemView.findViewById(R.id.tvAddress);
+
+        }
+
+    }
+
+    @Override
+    public RestaurantAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        mContext = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        View restaurantView = inflater.inflate(R.layout.item_restaurant, parent, false);
+        ViewHolder viewHolder = new ViewHolder(restaurantView);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(RestaurantAdapter.ViewHolder holder, int position) {
+        Restaurant restaurant = mRestaurants.get(position);
+        holder.tvName.setText(restaurant.name);
+        if (restaurant.isClosed) {
+            holder.tvOpen.setText("closed");
+            holder.tvOpen.setTextColor(Color.RED);
+        }
+        else {
+            holder.tvOpen.setText("open");
+            holder.tvOpen.setTextColor(Color.GREEN);
+        }
+        holder.tvOpen.setText(restaurant.isClosed? "closed" : "open");
+//        holder.tvDistance.setText(String.valueOf(restaurant.distance) + " mi");
+        holder.tvDistance.setText(new DecimalFormat("##.##").format(restaurant.distance) + " mi");
+        holder.tvPrice.setText(restaurant.price);
+        holder.tvReviewCount.setText(String.valueOf(restaurant.reviewCount) + " reviews");
+        holder.tvCategory.setText(restaurant.categories);
+        holder.tvAddress.setText(restaurant.address);
+        holder.ratingBar.setRating((float)restaurant.rating);
+        Picasso.with(mContext).load(restaurant.imageUrl).placeholder(R.mipmap.ic_launcher).transform(new RoundedCornersTransformation(10, 10)).into(holder.ivProfileImage);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mRestaurants.size();
+    }
+}
