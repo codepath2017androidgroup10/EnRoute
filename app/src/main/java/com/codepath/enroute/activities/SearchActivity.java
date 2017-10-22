5c5ad5cda411641ba760ae2d1596ca16e9494eaa
DroidSoul <huangw2@gmail.com>
2017-10-21 23:10:14 -0700

    added autocomplete search history

--- app/src/main/java/com/codepath/enroute/activities/SearchActivity.java
+++ app/src/main/java/com/codepath/enroute/activities/SearchActivity.java
@@ -1,6 +1,7 @@
 package com.codepath.enroute.activities;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.databinding.DataBindingUtil;
 import android.location.Location;
 import android.os.Bundle;
@@ -14,8 +15,10 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
-
+import android.widget.AutoCompleteTextView;
 import com.codepath.enroute.Manifest;
 import com.codepath.enroute.R;
 import com.codepath.enroute.connection.GoogleClient;
@@ -34,10 +37,16 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import cz.msebera.android.httpclient.Header;
 import io.fabric.sdk.android.Fabric;
 import permissions.dispatcher.NeedsPermission;
 import permissions.dispatcher.RuntimePermissions;
+
+import static android.R.id.input;
+import static java.security.AccessController.getContext;
 
 @RuntimePermissions
 public class SearchActivity extends AppCompatActivity {
@@ -47,8 +56,8 @@ public class SearchActivity extends AppCompatActivity {
     private ActivitySearchBinding mBinding;
     private String fromLocation = "";
     private String toLocation = "";
-    private EditText etToLocation;
-    private EditText etFromLocation;
+    private AutoCompleteTextView etToLocation;
+    private AutoCompleteTextView etFromLocation;
 
     static final String KEY_FROM_LAT = "FROM_LAT";
     static final String KEY_TO_LAT = "TO_LAT";
@@ -59,6 +68,9 @@ public class SearchActivity extends AppCompatActivity {
     static final String KEY_DESTINATION = "DESTINATION";
     private final GoogleClient googleClient = GoogleClient.getInstance();
     private FusedLocationProviderClient mFusedLocationClient;
+    SharedPreferences settingPreference;
+    private Set<String> toHistory;
+    private Set<String> fromHistory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,13 +79,19 @@ public class SearchActivity extends AppCompatActivity {
         YelpClient.getInstance();
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_search);
         setContentView(R.layout.activity_search);
+        etToLocation = findViewById(R.id.etTo);
+        etFromLocation = findViewById(R.id.etFrom);
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
         setUpViews();
     }
 
     private void setUpViews() {
         // TODO: Change it to get text from binding
-        etFromLocation = (EditText) findViewById(R.id.etFrom);
+        settingPreference = getSharedPreferences(String.valueOf(R.string.setting_preference), MODE_PRIVATE);
+        toHistory = settingPreference.getStringSet("toHistory", new HashSet());
+        fromHistory = settingPreference.getStringSet("fromHistory", new HashSet());
+        fromSetAutoCompleteSource();
+        toSetAutoCompleteSource();
         etFromLocation.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
@@ -92,7 +110,6 @@ public class SearchActivity extends AppCompatActivity {
                 }
             }
         });
-        etToLocation = (EditText) findViewById(R.id.etTo);
         etToLocation.requestFocus();
         etToLocation.addTextChangedListener(new TextWatcher() {
             @Override
@@ -132,10 +149,35 @@ public class SearchActivity extends AppCompatActivity {
     *
     * */
 
+    private void fromSetAutoCompleteSource() {
+//        AutoCompleteTextView textView = (AutoCompleteTextView) findViewById(R.id.etInput);
+        ArrayAdapter<String> fromAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, fromHistory.toArray(new String[fromHistory.size()]));
+        etFromLocation.setAdapter(fromAdapter);
+    }
+
+    private void toSetAutoCompleteSource() {
+//        AutoCompleteTextView textView = (AutoCompleteTextView) findViewById(R.id.etInput);
+        ArrayAdapter<String> toAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, toHistory.toArray(new String[toHistory.size()]));
+        etToLocation.setAdapter(toAdapter);
+    }
+
+
     public void onButtonSearch(View view) {
         fromLocation = etFromLocation.getEditableText().toString();
         toLocation = etToLocation.getEditableText().toString();
         SearchActivityPermissionsDispatcher.getCurrentLocationOfUserWithCheck(this);
+    }
+
+    private void toSavePrefs() {
+
+        settingPreference.edit().putStringSet("toHistory", toHistory).commit();
+
+    }
+
+    private void fromSavePrefs() {
+
+        settingPreference.edit().putStringSet("fromHistory", fromHistory).commit();
+
     }
 
     @NeedsPermission({Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION})
@@ -224,7 +266,16 @@ public class SearchActivity extends AppCompatActivity {
     }
 
     private void sendToMap(String encodedPolyLine, JSONObject jsonResponse) {
-
+        if (!toHistory.contains(toLocation)) {
+            toHistory.add(toLocation);
+            toSavePrefs();
+            toSetAutoCompleteSource();
+        }
+        if (!fromHistory.contains(fromLocation)) {
+            fromHistory.add(fromLocation);
+            fromSavePrefs();
+            fromSetAutoCompleteSource();
+        }
         Intent intent = new Intent(this, PlacesActivity.class);
         intent.putExtra(KEY_DIRECTIONS, encodedPolyLine);
         intent.putExtra(KEY_RESPONSE_JSON, jsonResponse.toString());
