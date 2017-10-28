59a5295144205594f978b9ae68a260f933d99675
Valli Vidhya Venkatesan <vallividhyavenkatesan@gmail.com>
2017-10-28 16:35:14 -0700

    Added Splash Screen

---
+++ app/src/main/java/com/codepath/enroute/activities/SplashScreenActivity.java
@@ -0,0 +1,81 @@
+package com.codepath.enroute.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.codepath.enroute.R;
+import com.daimajia.androidanimations.library.Techniques;
+import com.viksaa.sssplash.lib.activity.AwesomeSplash;
+import com.viksaa.sssplash.lib.cnst.Flags;
+import com.viksaa.sssplash.lib.model.ConfigSplash;
+
+
+/**
+ * Created by vidhya on 10/28/17.
+ */
+
+public class SplashScreenActivity extends AwesomeSplash {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void initSplash(ConfigSplash configSplash) {
+        getAndSetSplashValues(configSplash);
+    }
+
+    private void getAndSetSplashValues(ConfigSplash configSplash) {
+
+        //Customize Circular Reveal
+        configSplash.setBackgroundColor(R.color.colorGrey); //any color you want form colors.xml
+        configSplash.setAnimCircularRevealDuration(1000); //int ms
+        configSplash.setRevealFlagX(Flags.REVEAL_RIGHT);  //or Flags.REVEAL_LEFT
+        configSplash.setRevealFlagY(Flags.REVEAL_BOTTOM); //or Flags.REVEAL_TOP
+
+        //Choose LOGO OR PATH; if you don't provide String value for path it's logo by default
+
+        //Customize Logo
+        configSplash.setLogoSplash(R.drawable.launcher_128); //or any other drawable
+        configSplash.setAnimLogoSplashDuration(1000); //int ms
+        configSplash.setAnimLogoSplashTechnique(Techniques.Bounce); //choose one form Techniques (ref: https://github.com/daimajia/AndroidViewAnimations)
+
+
+        //Customize Path
+        //configSplash.setPathSplash(SyncStateContract.Constants.DROID_LOGO); //set path String
+        configSplash.setOriginalHeight(600); //in relation to your svg (path) resource
+        configSplash.setOriginalWidth(600); //in relation to your svg (path) resource
+        configSplash.setAnimPathStrokeDrawingDuration(500);
+        configSplash.setPathSplashStrokeSize(3); //I advise value be <5
+        configSplash.setPathSplashStrokeColor(R.color.colorWhitish); //any color you want form colors.xml
+        configSplash.setAnimPathFillingDuration(500);
+        configSplash.setPathSplashFillColor(R.color.colorWhite); //path object filling color
+
+
+        //Customize Title
+        configSplash.setTitleSplash("En Route");
+        configSplash.setTitleTextColor(R.color.colorAccent);
+        configSplash.setTitleTextSize(30f); //float value
+        configSplash.setAnimTitleDuration(500);
+        configSplash.setAnimTitleTechnique(Techniques.FlipInX);
+        configSplash.setTitleFont("fonts/Dosis-Book.ttf"); //provide string to your font located in assets/fonts/
+    }
+
+    @Override
+    public void animationsFinished() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startApp();
+            }
+        }, 500);
+    }
+
+    private void startApp() {
+        Intent intent = new Intent(this, SearchActivity.class);
+        startActivity(intent);
+    }
+}
